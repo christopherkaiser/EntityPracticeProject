7d9eec29f654f74282901057621319ef3990f6b8
Christopher Kaiser <ckaiser524@live.com>
2018-11-24 21:19:47 -0500

    test commit

--- NinjaDomain.DataModel/Class1.cs
+++ NinjaDomain.DataModel/Class1.cs
@@ -8,5 +8,6 @@ namespace NinjaDomain.DataModel
         public DbSet<Ninja> Ninjas { get; set; }
         public DbSet<Clan> Clans { get; set; }
         public DbSet<NinjaEquipment> Equipment { get; set; }
+
     }
 }
